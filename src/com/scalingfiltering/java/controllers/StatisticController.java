c4fd6035e05f0bb4d28d3c52c4ff1339924a0c0c
HouariZegai <houarizegai14@gmail.com>
2019-06-19 00:48:43 +0100

    Result - loading statistic (average error & hardware/calculate speed)

--- src/com/scalingfiltering/java/controllers/StatisticController.java
+++ src/com/scalingfiltering/java/controllers/StatisticController.java
@@ -1,5 +1,6 @@
 package com.scalingfiltering.java.controllers;
 
+import com.scalingfiltering.java.engine.Filtrage;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
@@ -25,11 +26,12 @@ public class StatisticController implements Initializable {
 
     private void loadLineChartError() {
         XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Series1");
+        series1.setName("Moyenne d'erreur");
 
         // Populating the series with data
-        for (int i = 1; i <= 12; i++) {
-            series1.getData().add(new XYChart.Data("" + i, i / 2));
+        for (int i = 0; i < Filtrage.moy_err.length; i++) {
+            series1.getData().add(new XYChart.Data(i + 1 + "", Filtrage.moy_err[i]));
+            System.out.println(Filtrage.moy_err[i]);
         }
 
         // Add the series (lines) to the chart
@@ -37,27 +39,28 @@ public class StatisticController implements Initializable {
     }
 
     private void loadBarChartHard() {
-        XYChart.Series series1 = new XYChart.Series();
-        XYChart.Series series2 = new XYChart.Series();
-        XYChart.Series series3 = new XYChart.Series();
+        XYChart.Series[] series = new XYChart.Series[6];
+        for(int i = 0; i < series.length; i++)
+            series[i] = new XYChart.Series();
 
-        series1.setName("CPU:i3 RAM:4GB");
-        series2.setName("CPU:i5 RAM:8GB");
-        series3.setName("CPU:i7 RAM:16GB");
+        series[0].setName("CPU:i3 RAM:4GB");
+        series[1].setName("CPU:i3 RAM:8GB");
+        series[2].setName("CPU:i5 RAM:4GB");
+        series[3].setName("CPU:i5 RAM:8GB");
+        series[4].setName("CPU:i7 RAM:8GB");
+        series[5].setName("CPU:i7 RAM:16GB");
 
         // insert the data to the series (bars)
 
-        series1.getData().addAll(
-                new XYChart.Data<>("Time Speed", 20));
-
-        series2.getData().addAll(
-                new XYChart.Data<>("Time Speed", 50));
-
-        series3.getData().addAll(
-                new XYChart.Data<>("Time Speed", 30));
+        series[0].getData().addAll(new XYChart.Data<>("Time Speed", 70));
+        series[1].getData().addAll(new XYChart.Data<>("Time Speed", 60));
+        series[2].getData().addAll(new XYChart.Data<>("Time Speed", 50));
+        series[3].getData().addAll(new XYChart.Data<>("Time Speed", 48));
+        series[4].getData().addAll(new XYChart.Data<>("Time Speed", 30));
+        series[5].getData().addAll(new XYChart.Data<>("Time Speed", 20));
 
         // add the series to the chart
-        barChartHardComp.getData().addAll(series1, series2, series3);
+        barChartHardComp.getData().addAll(series);
     }
 
     @FXML
